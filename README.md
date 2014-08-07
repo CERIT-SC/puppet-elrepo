488c0352b046b34276dc09541e614d5be19c8724
Vlastimil Holer <holer@ics.muni.cz>
2014-08-07 17:17:38 +0200

    Added .gitignore, metadatajson. Fixed documentation and module cleanup.

--- README.md
+++ README.md
@@ -14,4 +14,26 @@ Module has been tested on:
 
 Add repository
 
-    include elrepo
+```puppet
+include elrepo
+```
+
+Full configuration options:
+
+```puppet
+class { 'elrepo':
+  ensure          => present|absent,  # ensure state
+  version         => 6,               # system major release
+  proxy           => absent|'...',    # URL or absent
+  gpgcheck        => 1|0,             # check GPG signatures
+  gpgkey          => '...',           # local path to GPG key
+  enabled         => 1|0,             # enabled main repository
+  enabled_testing => 1|0,             # enabled testing repo.
+  enabled_kernel  => 1|0,             # enabled kernel repo.
+  enabled_extras  => 1|0,             # enabled extras repo.
+}
+```
+
+***
+
+CERIT Scientific Cloud, <support@cerit-sc.cz>
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,19 +1,15 @@
-# Class elrepo::params
-
 class elrepo::params {
-	$proxy					= absent
-	$ensure					= present
-	$gpgcheck				= 1
-	$gpgkey					= '/etc/pki/rpm-gpg/RPM-GPG-KEY-elrepo.org'
-	$enabled_elrepo			= 1
-	$enabled_elrepo_testing	= 0
-	$enabled_elrepo_kernel	= 0
-	$enabled_elrepo_extras	= 0
+  $proxy = absent
+  $ensure = present
+  $version = $::operatingsystemmajrelease
+  $gpgcheck = 1
+  $gpgkey = '/etc/pki/rpm-gpg/RPM-GPG-KEY-elrepo.org'
+  $enabled= 1
+  $enabled_testing = 0
+  $enabled_kernel = 0
+  $enabled_extras = 0
 
-	if $::osfamily == 'RedHat' and $::operatingsystem != 'Fedora' {
-		$version=split($::operatingsystemrelease,'[.]')
-		$majdistrelease=$version[0] #first component
-	} else {
-		fail("Unsupported OS (${::operatingsystem})")
-	}
+  unless $::osfamily != 'RedHat' or $::operatingsystem == 'Fedora' {
+    fail("Unsupported OS (${::operatingsystem})")
+  }
 }
